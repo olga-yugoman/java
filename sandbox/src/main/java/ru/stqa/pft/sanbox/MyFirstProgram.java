fb4805db4375ac9817350d921dfc9deb65668b54
Olga Yugoman <o.yugoman@quality-lab.ru>
2019-06-25 20:15:54 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
@@ -2,6 +2,28 @@ package ru.stqa.pft.sanbox;
 
 public class MyFirstProgram {
     public static void main(String[] args){
-        System.out.println("Hello, world!");
-	}
+        hello("world");
+        hello("user");
+        hello("www");
+
+        double a = 5;
+        System.out.println("Прощадь квадрата со стороной " + a + " = " + area(a));
+
+        double c = 5;
+        double d = 3;
+        System.out.println("Прощадь прямоугольника со сторонами " + c + " и " + d + " = " + area(c, d));
+
+    }
+
+	public static void hello(String somebody){
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double a){
+        return a * a;
+    }
+
+    public static double area(double a, double b){
+        return a * b;
+    }
 }
