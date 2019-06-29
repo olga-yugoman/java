7f9311465b4e51021d15236617936a365eb66411
Olga Yugoman <o.yugoman@quality-lab.ru>
2019-06-29 13:54:38 +0300

    Перенес в фикстуры логина и логаута из приложения

--- addressbook/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -12,10 +12,6 @@ public class GroupCreationTests {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://localhost/addressbook/");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -24,6 +20,10 @@ public class GroupCreationTests {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
@@ -37,11 +37,11 @@ public class GroupCreationTests {
     wd.findElement(By.name("group_footer")).sendKeys("test");
     wd.findElement(By.name("submit")).click();
     wd.findElement(By.linkText("group page")).click();
-    wd.findElement(By.linkText("Logout")).click();
   }
 
   @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
+    wd.findElement(By.linkText("Logout")).click();
     wd.quit();
   }
 
