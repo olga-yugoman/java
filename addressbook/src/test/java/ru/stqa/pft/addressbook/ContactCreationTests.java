25f57e6d998f55d20ef5d867874b8a6200d0410f
Olga Yugoman <o.yugoman@quality-lab.ru>
2019-06-29 15:05:44 +0300

    Контакты. Тест на создание контакта
    Записать в Katalon Recorder тест для создания нового контакта в адресной книге
    Перевести тест на язык Java и перенести его в среду разработки (при экспорте использовать формат "Java/TestNG")
    Выделить из кода теста вспомогательные методы, в том числе метод заполнения формы.
    Параметризовать метод заполнения формы.
    Сгруппировать параметры метода заполнения формы в объект, сделать вспомогательный "модельный" класс ContactData для представления таких объектов.
    И снова можно уложить результаты работы в репозиторий. Делайте это после каждого успешного шага. А если что-то сломалось -- можно отменить изменения, вернуться к предыдущему работающему состоянию, и попробовать ещё раз.

---
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -0,0 +1,96 @@
+package ru.stqa.pft.addressbook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ContactCreationTests {
+    private WebDriver wd;
+
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/");
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//input[@value='Login']")).click();
+    }
+
+    @Test
+    public void testContactCreationTests() throws Exception {
+        initContactCreation();
+        fillContactForm(new ContactData("Olga", "Yugoman", "address", "0981223344", "mail@mail.ru"));
+        submitContactForm();
+        returntoHomePage();
+    }
+
+    private void returntoHomePage() {
+        wd.findElement(By.linkText("home page")).click();
+    }
+
+    private void submitContactForm() {
+        wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+    }
+
+    private void fillContactForm(ContactData contactData) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getSurname());
+        wd.findElement(By.name("address")).click();
+        wd.findElement(By.name("address")).clear();
+        wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+        wd.findElement(By.name("home")).click();
+        wd.findElement(By.name("home")).clear();
+        wd.findElement(By.name("home")).sendKeys(contactData.getHomePhone());
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    }
+
+    private void initContactCreation() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() throws Exception {
+        wd.findElement(By.linkText("Logout")).click();
+        wd.quit();
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            wd.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
---
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstName;
+    private final String surname;
+    private final String address;
+    private final String homePhone;
+    private final String email;
+
+    public ContactData(String firstName, String surname, String address, String homePhone, String email) {
+        this.firstName = firstName;
+        this.surname = surname;
+        this.address = address;
+        this.homePhone = homePhone;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getHomePhone() {
+        return homePhone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
