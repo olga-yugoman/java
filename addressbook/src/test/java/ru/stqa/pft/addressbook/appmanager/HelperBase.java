285ee2d15c5964154e09822aaf69df26690b3a6f
Olga Yugoman <o.yugoman@quality-lab.ru>
2019-07-02 17:53:03 +0300

    Лекция 3.5 Оставляем дефолтные значения в форме
    Если передан null, то ничего не печатать в поле.

--- addressbook/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -18,8 +18,10 @@ public class HelperBase {
 
     protected void type(By locator, String text) {
         click(locator);
-        wd.findElement(locator).clear();
-        wd.findElement(locator).sendKeys(text);
+        if (text != null) {
+            wd.findElement(locator).clear();
+            wd.findElement(locator).sendKeys(text);
+        }
     }
 
     private boolean isElementPresent(By by) {
