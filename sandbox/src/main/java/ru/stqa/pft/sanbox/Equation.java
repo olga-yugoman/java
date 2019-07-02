a3cd3cf0518ac0f38a8e25800c922601a754485c
Olga Yugoman <o.yugoman@quality-lab.ru>
2019-07-02 17:03:50 +0300

    Лекция 3.2 Виды конструкции if-else

--- sandbox/src/main/java/ru/stqa/pft/sanbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/Equation.java
@@ -15,16 +15,23 @@ public class Equation {
 
         double d = b*b - 4*a*c;
 
-        if (d > 0) {
-            n = 2;
-        } else {
-            if (d == 0) {
+        if (a != 0) {
+            if (d > 0) {
+                n = 2;
+            } else if (d == 0) {
                 n = 1;
             } else {
                 n = 0;
             }
+
+        } else if (b != 0) {
+                n = 1;
+        } else if (c != 0) {
+            n = 0;
+        } else {
+            n = -1;
         }
-   }
+    }
 
    public int rootNumber() {
         return n;
