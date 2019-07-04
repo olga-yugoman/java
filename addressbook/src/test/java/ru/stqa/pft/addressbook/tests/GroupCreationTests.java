b08b07de9ede9261d2f7392cc5592bbd99490113
Olga Yugoman <o.yugoman@quality-lab.ru>
2019-07-04 14:20:47 +0300

    Найти максимальный id при помощи компаратора

--- addressbook/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -25,12 +26,13 @@ public class GroupCreationTests extends TestBase {
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max = 0;
-        for (GroupData g : after) {
-            if (g.getId() > max) {
-                max = g.getId();
+        Comparator<? super GroupData> byId = new Comparator<GroupData>() {
+            @Override
+            public int compare(GroupData o1, GroupData o2) {
+                return Integer.compare(o1.getId(), o2.getId());
             }
-        }
+        };
+        int max = after.stream().max(byId).get().getId();
         group.setId(max);
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
