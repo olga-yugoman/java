7ad19436ce78bd509a82c7f048399a23773dbe29
Olga Yugoman <o.yugoman@quality-lab.ru>
2019-07-04 14:03:12 +0300

    Лекция 4.8. Поиск максимального элемента в коллекции
    Через цикл

--- addressbook/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,12 +1,12 @@
 package ru.stqa.pft.addressbook.model;
 
 public class GroupData {
-    private final String id;
+    private int id;
     private final String name;
     private final String header;
     private final String footer;
 
-    public GroupData(String id, String name, String header, String footer) {
+    public GroupData(int id, String name, String header, String footer) {
         this.id = id;
         this.name = name;
         this.header = header;
@@ -14,13 +14,17 @@ public class GroupData {
     }
 
     public GroupData(String name, String header, String footer) {
-        this.id = null;
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
 
-    public String getId() {
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
         return id;
     }
 
@@ -51,13 +55,13 @@ public class GroupData {
 
         GroupData groupData = (GroupData) o;
 
-        if (id != null ? !id.equals(groupData.id) : groupData.id != null) return false;
+        if (id != groupData.id) return false;
         return name != null ? name.equals(groupData.name) : groupData.name == null;
     }
 
     @Override
     public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
+        int result = id;
         result = 31 * result + (name != null ? name.hashCode() : 0);
         return result;
     }
