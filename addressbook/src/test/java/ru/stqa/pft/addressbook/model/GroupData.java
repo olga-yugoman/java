1c7f812f5c7ba70669132e8281391c599df5b9f6
Olga Yugoman <o.yugoman@quality-lab.ru>
2019-07-04 14:39:27 +0300

    Добавлена сортировка списков для сравнения.
    Теперь снова сравниваем списки, а не множества.

--- addressbook/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -14,7 +14,7 @@ public class GroupData {
     }
 
     public GroupData(String name, String header, String footer) {
-        this.id = 0;
+        this.id = Integer.MAX_VALUE;
         this.name = name;
         this.header = header;
         this.footer = footer;
@@ -55,14 +55,11 @@ public class GroupData {
 
         GroupData groupData = (GroupData) o;
 
-        if (id != groupData.id) return false;
         return name != null ? name.equals(groupData.name) : groupData.name == null;
     }
 
     @Override
     public int hashCode() {
-        int result = id;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
+        return name != null ? name.hashCode() : 0;
     }
 }
