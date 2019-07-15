540791602ac6331b18fd55902e795f3851465297
Olga Yugoman <o.yugoman@quality-lab.ru>
2019-07-15 21:41:01 +0300

    Added dataproviderfor groups

--- addressbook/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,20 +1,32 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-    @Test
-    public void testGroupCreation() throws Exception {
-        app.goTo().groupPage();
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<>();
+        list.add(new Object[] {new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
+        list.add(new Object[] {new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
+        list.add(new Object[] {new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
+        return list.iterator();
+    }
 
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) throws Exception {
+        app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("test2");
         app.group().create(group);
         assertThat(app.group().count(), equalTo(before.size() + 1));
         Groups after = app.group().all();
