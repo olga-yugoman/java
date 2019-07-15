6bf7d45911563d56544a846b57daf2ed7a360267
Olga Yugoman <o.yugoman@quality-lab.ru>
2019-07-15 20:36:20 +0300

    Лекция 6.1 Узнать путь к рабочей директории
    Проверить существование файла по указанному пути.

--- addressbook/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -12,12 +14,22 @@ public class ContactCreationTests extends TestBase {
     @Test
     public void testContactCreation() throws Exception {
         Contacts before = app.contact().all();
+        File photo = new File("src/test/resources/stud.png");
         ContactData contact = new ContactData().withFirstName("Olga").withSurname("Yugoman")
-                .withAddress("address").withHomePhone("0981223344").withEmail("mail@mail.ru").withGroup("test1");
+                .withAddress("address").withHomePhone("0981223344").withEmail("mail@mail.ru").withPhoto(photo);
         app.contact().create(contact, true);
         assertThat(app.contact().count(), equalTo(before.size() + 1));
         Contacts after = app.contact().all();
         assertThat(after, equalTo(
                 before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+    }
+
+    @Test
+    public void testCurrentDir() {
+        File currentDir = new File(".");
+        System.out.println(currentDir.getAbsolutePath());
+        File photo = new File("src/test/resources/stud.png");
+        System.out.println(photo.getAbsolutePath());
+        System.out.println(photo.exists());
     }
 }
