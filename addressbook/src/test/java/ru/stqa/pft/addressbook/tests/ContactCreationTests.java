1ef8c599dc929765ca44dbd640ab63cf10d01c64
Olga Yugoman <o.yugoman@quality-lab.ru>
2019-07-18 20:44:12 +0300

    ДЗ 13 Добавлен датапровайдер в тест на создание контактов

--- addressbook/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,23 +1,44 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testContactCreation() throws Exception {
+    @DataProvider
+    public Iterator<Object[]> contacts() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+        String xml = "";
+        String line = reader.readLine();
+        while (line != null) {
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+        return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test(dataProvider = "contacts")
+    public void testContactCreation(ContactData contact) throws Exception {
         Contacts before = app.contact().all();
         File photo = new File("src/test/resources/stud.png");
-        ContactData contact = new ContactData().withFirstName("Olga").withSurname("Yugoman")
-                .withAddress("address").withHomePhone("0981223344").withEmail("mail@mail.ru").withPhoto(photo);
-        app.contact().create(contact, true);
+        app.contact().create(contact.withPhoto(photo), true);
         assertThat(app.contact().count(), equalTo(before.size() + 1));
         Contacts after = app.contact().all();
         assertThat(after, equalTo(
