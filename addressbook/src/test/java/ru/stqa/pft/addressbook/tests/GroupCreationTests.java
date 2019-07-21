29a201f2450dc7c1f7a922aabd990bac9fc962cd
Olga Yugoman <o.yugoman@quality-lab.ru>
2019-07-21 19:56:19 +0300

    Added logging

--- addressbook/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,8 @@
 package ru.stqa.pft.addressbook.tests;
 
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
--- addressbook/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,11 +1,21 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
 
     protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
@@ -19,4 +29,13 @@ public class TestBase {
         app.stop();
     }
 
+    @BeforeMethod(alwaysRun = true)
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info("Stop test " + m.getName());
+    }
 }
