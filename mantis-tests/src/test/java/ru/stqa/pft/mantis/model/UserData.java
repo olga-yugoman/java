ab36b2a7ab076ecf0050812eaaa67fe973a0a56a
Olga Yugoman <o.yugoman@quality-lab.ru>
2019-07-22 22:21:14 +0300

    Added possibility to connect to DB with users

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/model/UserData.java
@@ -0,0 +1,56 @@
+package ru.stqa.pft.mantis.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "mantis_user_table")
+public class UserData {
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public UserData withId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public UserData setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public UserData setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "UserData{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/HbConnectionTest.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import ru.stqa.pft.mantis.model.UserData;
+
+import java.util.List;
+
+public class HbConnectionTest {
+
+    private SessionFactory sessionFactory;
+
+    @BeforeClass
+    protected void setUp() throws Exception {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+
+        try {
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+
+    @Test
+    public void testHbConnectionMantis() {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<UserData> result = session.createQuery("from UserData").list();
+        session.getTransaction().commit();
+        session.close();
+
+        for (UserData user : result) {
+            System.out.println(user);
+        }
+    }
+}
