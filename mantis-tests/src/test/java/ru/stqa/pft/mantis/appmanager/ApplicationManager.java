3deb897af32f7a465541ad35e57b14d77fd20ee2
Olga Yugoman <o.yugoman@quality-lab.ru>
2019-07-22 16:43:01 +0300

    Лекция8.3 Добавлен тест на логин по Http

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -40,4 +40,12 @@ public class ApplicationManager {
     public void stop() {
         wd.quit();
     }
+
+    public HttpSession newSession() {
+        return new HttpSession(this);
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
 }
